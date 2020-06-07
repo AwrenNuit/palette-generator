6985841f4411f220ae89b2ea8820a7ba3a0bb5f5
AwrenNuit <a.rennaker@yahoo.com>
2020-06-07 11:30:37 -0500

    re-arranged some bits for readability and performance

--- scripts.js
+++ scripts.js
@@ -1,21 +1,27 @@
+const palette = ['first', 'second', 'third', 'fourth', 'fifth'];
+const range = [];
+setRange();
 document.getElementById("generate-btn").addEventListener("click", commenceTheming);
 
-function randomColor() {
-  let range = [];
-  let red, green, blue;
-  for(let i=0; i<255; i++) {
-    range.push(i);
-  }
-  red = range[Math.floor(Math.random() * range.length)];
-  green = range[Math.floor(Math.random() * range.length)];
-  blue = range[Math.floor(Math.random() * range.length)];
+function assignRandomColor() {
+  const red = setColor();
+  const green = setColor();
+  const blue = setColor();
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
 function commenceTheming() {
-  document.getElementById("first").style.backgroundColor = randomColor();
-  document.getElementById("second").style.backgroundColor = randomColor();
-  document.getElementById("third").style.backgroundColor = randomColor();
-  document.getElementById("fourth").style.backgroundColor = randomColor();
-  document.getElementById("fifth").style.backgroundColor = randomColor();
+  for(let num of palette) {
+    document.getElementById(num).style.backgroundColor = assignRandomColor();
+  }
+}
+
+function setColor() {
+  return range[Math.floor(Math.random() * range.length)];
+}
+
+function setRange() {
+  for(let i=0; i<255; i++) {
+    range.push(i);
+  }
 }
