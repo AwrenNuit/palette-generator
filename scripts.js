fbac2870190f9c6e5896614d1dee4087bee06125
AwrenNuit <a.rennaker@yahoo.com>
2020-06-06 23:32:51 -0500

    added click handler to button to generate random colors for each square

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+document.getElementById("generate-btn").addEventListener("click", commenceTheming);
+
 function randomColor() {
   let range = [];
   let red, green, blue;
@@ -8,4 +10,12 @@ function randomColor() {
   green = range[Math.floor(Math.random() * range.length)];
   blue = range[Math.floor(Math.random() * range.length)];
   return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function commenceTheming() {
+  document.getElementById("first").style.backgroundColor = randomColor();
+  document.getElementById("second").style.backgroundColor = randomColor();
+  document.getElementById("third").style.backgroundColor = randomColor();
+  document.getElementById("fourth").style.backgroundColor = randomColor();
+  document.getElementById("fifth").style.backgroundColor = randomColor();
 }
